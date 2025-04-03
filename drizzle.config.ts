a7240bc693c21619592b3588629a1df7d34feb45
michaelma7 <michaelma16688@gmail.com>
2025-04-03 12:06:39 -0400

    db setup

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env' });
+
+export default defineConfig({
+  schema: './db/schema.ts',
+  out: './migrations',
+  dialect: 'turso',
+  dbCredentials: {
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+  verbose: true,
+  strict: true,
+});
