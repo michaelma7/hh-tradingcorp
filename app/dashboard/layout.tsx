8663d73ae7dd7eb5cf3a532a81e6aaeb1619051c
michaelma7 <michaelma16688@gmail.com>
2025-10-07 13:25:38 -0400

    added layouts for home/dashboard and auth pages

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,14 @@
 import Sidebar from '../../components/Sidebar';
+import Nav from '@/components/Nav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex h-screen flex-col md:flexrow md:overflow-hidden'>
       <div className='w-full flex-none md:w-64'>
         <Sidebar />
+      </div>
+      <div>
+        <Nav />
       </div>
       <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
     </div>
