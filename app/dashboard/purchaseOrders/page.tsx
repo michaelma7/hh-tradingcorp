275039405907bb3ad9febf6f75eb98d6ef38d2c9
michaelma7 <michaelma16688@gmail.com>
2025-10-17 15:06:50 -0400

    created purchase orders management page

--- app/dashboard/purchaseOrders/page.tsx
+++ app/dashboard/purchaseOrders/page.tsx
@@ -1,18 +1,44 @@
 import { getAllPurchaseOrders } from '@/actions/purchaseOrders';
 import Link from 'next/link';
+import NewPurchaseOrderModal from '@/components/NewPurchaseOrderModal';
+import { CirclePlus } from 'lucide-react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableRow,
+  TableHeader,
+} from '@heroui/react';
 
 export default async function PurchaseOrders() {
   const purchaseOrders = await getAllPurchaseOrders();
 
   return (
     <div>
-      {purchaseOrders.map((purchaseOrder) => {
-        <div key={purchaseOrder.id}>
-          <Link href={`/dashboard/purchaseOrders/${purchaseOrder.id}`}>
-            {purchaseOrder.name}
-          </Link>
-        </div>;
-      })}
+      <NewPurchaseOrderModal />
+      <Table>
+        <TableHeader>
+          <TableColumn>Purchase Order Id</TableColumn>
+          <TableColumn>Order Date</TableColumn>
+          <TableColumn>Status</TableColumn>
+          <TableColumn>See More Details</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {purchaseOrders.map((purchaseOrder) => {
+            <TableRow key={purchaseOrder.key}>
+              <TableCell>{purchaseOrder.id}</TableCell>
+              <TableCell>{purchaseOrder.orderDate}</TableCell>
+              <TableCell>{purchaseOrder.status}</TableCell>
+              <TableCell>
+                <Link href={`/dashboard/purchaseOrders/${purchaseOrder.id}`}>
+                  <CirclePlus size={11} />
+                </Link>
+              </TableCell>
+            </TableRow>;
+          })}
+        </TableBody>
+      </Table>
     </div>
   );
 }
