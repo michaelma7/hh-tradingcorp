1ea0af22264c33661dd0f138ff1cae4c48efbc4a
michaelma7 <michaelma16688@gmail.com>
2025-10-17 15:06:20 -0400

    created orders management page

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,16 +1,43 @@
 import { getAllOrders } from '@/actions/orders';
+import NewOrderModal from '@/components/NewOrderModal';
 import Link from 'next/link';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableRow,
+  TableHeader,
+} from '@heroui/react';
 
 export default async function Orders() {
   const orders = await getAllOrders();
 
   return (
     <div>
-      {orders.map((order) => {
-        <div key={order.id}>
-          <Link href={`/dashboard/orders/${order.id}`}>{order.name}</Link>
-        </div>;
-      })}
+      <NewOrderModal />
+      <Table>
+        <TableHeader>
+          <TableColumn>Order Name</TableColumn>
+          <TableColumn>Customer Name</TableColumn>
+          <TableColumn>Total</TableColumn>
+          <TableColumn>Delivered?</TableColumn>
+          <TableColumn>See More Details</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {orders.map((order) => {
+            <TableRow key={order.key}>
+              <TableCell>{order.name}</TableCell>
+              <TableCell>{order.customer}</TableCell>
+              <TableCell>{order.totalCents * 100}</TableCell>
+              <TableCell>{order.status}</TableCell>
+              <TableCell>
+                <Link href={`/dashboard/orders/${order.id}`}>More Details</Link>
+              </TableCell>
+            </TableRow>;
+          })}
+        </TableBody>
+      </Table>
     </div>
   );
 }
