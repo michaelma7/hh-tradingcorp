c3bbc32c50a20a9e03d05ebd027a16a3df5d7202
michaelma7 <michaelma16688@gmail.com>
2026-02-19 10:22:50 -0500

    chore: added typing to layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-export default function AuthLayout({ children }) {
+export default function AuthLayout({ children }: { children: any }) {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-[400px] mx-auto'>{children}</div>
