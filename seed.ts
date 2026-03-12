e3c06264d031c45feb15567a99b2fd65152f46f2
michaelma7 <michaelma16688@gmail.com>
2026-03-12 10:40:38 -0400

    update seed to add dummy data for all tables

--- seed.ts
+++ seed.ts
@@ -1,5 +1,12 @@
 import { db } from '@/db/db';
-import { users, products, manufacturers, customers } from '@/db/schema';
+import {
+  users,
+  products,
+  manufacturers,
+  customers,
+  orders,
+  purchaseOrders,
+} from '@/db/schema';
 import bcrypt from 'bcrypt';
 
 const seedDb = async () => {
@@ -8,10 +15,10 @@ const seedDb = async () => {
     if (!newUser) {
       console.error('create an account first');
       const pw = await bcrypt.hash('Hhtradingcorp1!', 10);
-      const newAdmin = await db
+      const [newAdmin] = await db
         .insert(users)
         .values({ email: 'aznxsyko@gmail.com', password: `${pw}` })
-        .returning();
+        .returning({ id: users.id });
       console.log('inserted user', newAdmin);
     }
     const newManufacturers = await db
@@ -43,15 +50,53 @@ const seedDb = async () => {
       .values([
         {
           name: 'michael',
-          location: 'lul',
+          location: 'flushing',
         },
         {
           name: 'yizhou',
           location: 'mucho',
         },
       ])
-      .returning();
+      .returning({ id: customers.id });
     console.log('inserted customers', newCustomers);
+    const newOrders = await db
+      .insert(orders)
+      .values([
+        {
+          name: 'test',
+          createdById: '1b0f9b35-5657-4398-adfe-78aa57ca6bf1',
+          customerId: `${newCustomers[0].id}`,
+          totalCents: 0,
+          status: false,
+        },
+        {
+          name: 'test2',
+          createdById: '1b0f9b35-5657-4398-adfe-78aa57ca6bf1',
+          customerId: `${newCustomers[0].id}`,
+          totalCents: 1000,
+          status: true,
+        },
+      ])
+      .returning();
+    console.log('inserted orders', newOrders);
+    const newPurchaseOrder = await db
+      .insert(purchaseOrders)
+      .values([
+        {
+          orderDate: '2026-2-15',
+          status: 'pending',
+          shippingInfo: 'n/a',
+          shipper: 'china',
+        },
+        {
+          orderDate: '2026-1-13',
+          status: 'received',
+          shippingInfo: '1zasd23258d890789asdf',
+          shipper: 'hk',
+        },
+      ])
+      .returning();
+    console.log('inserted purchase orders', newPurchaseOrder);
   } catch (err) {
     console.error('failed db seeding', err);
   }
