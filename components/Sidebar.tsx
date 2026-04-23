4474d8336e1fcb39d461c26cc33c826832a662eb
michaelma7 <michaelma16688@gmail.com>
2026-04-23 13:24:19 -0400

    updated links to be role bound

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,14 +3,18 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { Button } from '@heroui/react';
 import { signout } from '@/actions/auth';
+import { Roles, hierarchy } from '@/rbac/permissions';
+import { UserContext } from '@/providers/CurrentUserProvider';
+import { useContext } from 'react';
 
 const links = [
-  { route: '/dashboard', name: 'Home 首页' },
-  { route: '/dashboard/orders', name: '订单' },
-  { route: '/dashboard/products', name: ' 产品' },
-  { route: '/dashboard/manufacturers', name: '生产厂家' },
-  { route: '/dashboard/customers', name: '顾客' },
-  { route: '/dashboard/purchaseOrders', name: '库存订单' },
+  { route: '/dashboard', name: 'Home 首页', role: Roles.user },
+  { route: '/dashboard/orders', name: '订单', role: Roles.mod },
+  { route: '/dashboard/products', name: ' 产品', role: Roles.mod },
+  { route: '/dashboard/manufacturers', name: '生产厂家', role: Roles.mod },
+  { route: '/dashboard/customers', name: '顾客', role: Roles.mod },
+  { route: '/dashboard/purchaseOrders', name: '库存订单', role: Roles.mod },
+  { route: '/users', name: 'Users', role: Roles.admin },
 ];
 
 const isActive = (path: string, route: string) => {
@@ -19,12 +23,16 @@ const isActive = (path: string, route: string) => {
 };
 
 export default function Sidebar() {
+  const role = useContext(UserContext)!.currentUser.role;
   const path = usePathname();
+  const allowedPaths = links.filter((link) => {
+    return hierarchy.indexOf(link.role) <= hierarchy.indexOf(role);
+  });
   const active = 'bg-secondary hover:bg-secondary-300';
   return (
     <div className='w-full h-full p-3 relative'>
       <div className='grid gap-2'>
-        {links.map((link) => (
+        {allowedPaths.map((link) => (
           <div className='w-full ' key={link.route}>
             <Link className='' href={link.route}>
               <div
