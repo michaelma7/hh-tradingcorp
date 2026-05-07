1b479f2de41f7110428e4a96b45d29bb53887732
michaelma7 <michaelma16688@gmail.com>
2026-05-07 13:44:21 -0400

    added cookie checks for role and removed ability to access registering

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,16 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
+import { hierarchy } from './rbac/permissions';
 
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
+  const role = request.cookies.get('rbac')?.value ?? 'user';
+  const session = request.cookies.get('session')?.value ?? null;
+  // remove ability to register
+  if (request.nextUrl.pathname.startsWith('/register'))
+    return NextResponse.redirect(new URL('/', request.url));
+
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    if (!request.cookies.has('session')) {
+    if (!request.cookies.has('session') || !session) {
       return NextResponse.redirect(new URL('/signin', request.url));
     }
   }
@@ -11,8 +18,12 @@ export function proxy(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
+
+  if (request.nextUrl.pathname.startsWith('/users') && role !== 'admin') {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
